cabd5a8148a697f502e4cf6b23b4a83eaf65085e
AlifHossain27 <alifh044@gmail.com>
2024-02-16 19:51:17 +0600

    Adding CSS to Django Project

--- initproject.py
+++ initproject.py
@@ -11,8 +11,26 @@ class InitializeProject:
         self.css_type: str = "CSS"
         self.ui_lib: str = "None"
 
+    def css_menu(self):
+        css_options = [ "CSS", "TailwindCSS", "Quit" ]
+        css_menu = TerminalMenu(css_options, title= "Choose a CSS Library")
+
+        while not self.quitting:
+            css_options_index = css_menu.show()
+            css_options_choice = css_options[css_options_index]
+
+            if css_options_choice == "CSS":
+                self.css_type = "CSS"
+                break
+            if css_options_choice == "TailwindCSS":
+                self.css_type = "TailwindCSS"
+                break
+            if css_options_choice == "Quit":
+                self.quitting = True
+
 
     def create_django_project(self) -> None:
+        self.css_menu()
         with yaspin() as loader:
             loader.text = "Creating a new Django Project"
             loader.color = "green"
@@ -26,6 +44,8 @@ class InitializeProject:
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>{% block head_title %}{% endblock %}</title>
         {% block extra_head %}
         {% endblock %}
@@ -37,11 +57,56 @@ class InitializeProject:
 '''
             loader.text = "Adding Template"
             loader.color = "green"
+            # Creating templates folder
             os.mkdir("templates")
-            os.mkdir("static")
+            # Creating base.html file in templates folder
             os.system("cd templates && touch base.html")
+            # Writing base html
             with open(f"{self.cwd}/templates/base.html", 'w') as file:
                 file.writelines(base_html)
+            # Adding Template files path to settings.py
+            with open(f'{self.cwd}/core/settings.py', 'r') as file:
+                lines = file.readlines()
+            for i, line in enumerate(lines):
+                    if '"DIRS": []' in line:
+                        lines[i] = '        "DIRS": [BASE_DIR / "templates"],\n'
+                        break
+            with open(f'{self.cwd}/core/settings.py', 'w') as file:
+                file.writelines(lines)
+            loader.ok("✔")
+    
+    def css_setup(self) -> None:
+        with yaspin() as loader:
+            loader.text = "Adding CSS"
+            loader.color = "green"
+            if self.css_type == "CSS":
+                # Creating static folder
+                os.mkdir("static")
+                # Creating style.css in static folder
+                os.system("cd static && touch style.css")
+                # Adding Static files path to settings.py
+                with open(f'{self.cwd}/core/settings.py', 'r') as file:
+                    lines = file.readlines()
+                for i, line in enumerate(lines):
+                    if '# Static files (CSS, JavaScript, Images)' in line:
+                        tailwindcdn = 'STATICFILES_DIRS = [BASE_DIR / "static"]'
+                        lines.insert(i + 1, tailwindcdn + '\n')
+                        break
+                with open(f'{self.cwd}/core/settings.py', 'w') as file:
+                    file.writelines(lines)
+            if self.css_type == "TailwindCSS":
+                # Adding TailwindCSS CDN to base.html file
+                with open(f'{self.cwd}/templates/base.html', 'r') as file:
+                    lines = file.readlines()
+                for i, line in enumerate(lines):
+                    if '<meta name="viewport" content="width=device-width, initial-scale=1.0">' in line:
+                        tailwindcdn = '        <script src="https://cdn.tailwindcss.com"></script>'
+                        lines.insert(i + 1, tailwindcdn + '\n')
+                        break
+                with open(f'{self.cwd}/templates/base.html', 'w') as file:
+                    file.writelines(lines)
+            else:
+                pass
             loader.ok("✔")
 
     def run(self) -> None:
@@ -57,6 +122,7 @@ class InitializeProject:
             if main_options_choice == "Django":
                 self.create_django_project()
                 self.create_frontend_templates()
+                self.css_setup()
                 break
 
 
