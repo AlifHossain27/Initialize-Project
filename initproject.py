d1887f53cfab299bc3e5bd3dd97e65177f786656
AlifHossain27 <alifh044@gmail.com>
2024-02-16 17:24:16 +0600

    Initial commit

---
+++ initproject.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import os
+from yaspin import yaspin
+from simple_term_menu import TerminalMenu
+
+class InitializeProject:
+    def __init__(self) -> None:
+        pass
+
+    def run(self) -> None:
+        pass
+
+
+if __name__ == '__main__':
+    InitializeProject().run()
